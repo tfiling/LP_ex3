0dc60ae48aab4fe83e963548a2d798a9f2dd58b8
miamiHits <yardench@gmail.com>
2018-05-04 14:37:08 +0300

    almost DONE Task1

--- ex3.pl
+++ ex3.pl
@@ -20,41 +20,123 @@ and( 0, 0, 0).
 and( 0, 1, 0).
 and( 1, 0, 0).
 and( 1, 1, 1).
+
+dec2bin(0,[0]).
+dec2bin(1,[1]).
+dec2bin(N,L):- 
+    N > 1,
+    X is N mod 2,
+    Y is N // 2,  
+    dec2bin(Y,L1),
+    L = [X|L1].
+
+my_flatten([], []).
+my_flatten([A|B],L) :- 
+    my_flatten(B,B1), !, 
+    append(A,B1,L).
+my_flatten([A|B],[A|B1]) :- 
+    my_flatten(B,B1).
+
+zeroToMinusOne([], []).
+zeroToMinusOne([0|Rest], [-1|RestNew]):-
+        zeroToMinusOne(Rest, RestNew).
+zeroToMinusOne([1|Rest], [1|RestNew]):-
+        zeroToMinusOne(Rest, RestNew).
 /* Part A */
 
 /*task 1*/
 %sum_equals(+,+,-)
-%for Even vectors case
 sum_equals(Sum,Numbers,CNF):-
-    addVectors(Numbers, CNF),
-    sat(CNF),
-    integerRep(CNF, Sum2),
-    Sum == Sum2.
+    addVectors(Numbers, CNF, ResList), !,
+    my_flatten(ResList, LastVector),
+    dec2bin(Sum, BinSum),
+    reverse(BinSum, NewBinSum),
+    length(LastVector, N),
+    length(NewBinSum, M),
+    N > M,
+    abs(N-M, Val),
+    length(Block, Val),
+    paddZero(Block),
+    append(NewBinSum,Block, BinSumFinal),
+    writeln('BinSumFinal is:' + BinSumFinal),
+    zeroToMinusOne(BinSumFinal, BinSumMinus),
+    writeln('BinSumMinus is:' + BinSumMinus),
+    mapVals(BinSumMinus, LastVector),
+    writeln('LastVec is:' + LastVector).
 
-addVectors([_],[]).
 
-addVectors([X,Y|Numbers], CNF):-
-    add(X,Y,0,Sum1,CNF1),
-    addVectors([Sum1|Numbers], CNF2),
+mapVals([_],[_]).
+mapVals([B|BinSumMinus], [X|LastVector]):-
+    X = B,
+    mapVals(BinSumMinus, LastVector).
+   
+
+    
+    % sat(CNF).
+
+%addVectors (+,-)
+addVectors([_],[],[]).
+
+addVectors([X,Y], CNF, [Sum1|ResList]):-
+    length(X, N),
+    length(Y, M),
+    N > M, 
+    writeln('Last 2 elements'),
+    abs(N-M, Val),
+    length(Block, Val),
+    paddZero(Block),
+    append(Y,Block, NewY),  
+    add(X,NewY,0,Sum1, CNF1),  
+    writeln('Sum OF 2 vectors is: ' + Sum1),   
+    addVectors([Sum1], CNF2, ResList),
     append(CNF1, CNF2, CNF).
 
+addVectors([X,Y|Numbers], CNF, ResList):-
+    length(X, N),
+    length(Y, M),
+    N > M, 
+    abs(N-M, Val),
+    length(Block, Val),
+    paddZero(Block),
+    append(Y,Block, NewY),  
+    add(X,NewY,0,Sum1, CNF1),  
+    addVectors([Sum1|Numbers], CNF2, ResList),
+    append(CNF1, CNF2, CNF).
+
+addVectors([X,Y|Numbers], CNF, ResList):-
+    length(X, N),
+    length(Y, M),
+    N==M,
+    add(X,Y,0,Sum1,CNF1),
+    addVectors([Sum1|Numbers], CNF2, ResList),
+    append(CNF1, CNF2, CNF).
+
+    
+paddZero([]).    
+paddZero([0|Rest]):- 
+    paddZero(Rest).
+
+
+% if_then_else(Condition, Action1, Action2) :- 
+%     Condition, !, Action1.  
+% if_then_else(Condition, Action1, Action2) :- 
+%     Action2.
+
 %we did in class, SUM of 2 Bit Vectors
+% add(+, +, -, -, -)
 add([], [], C, [C], []).
 
-add([X|Xs], [Y|Ys], C, [S|Sum], CNF):-
+add([X|Xs], [Y|Ys], C, [S|Sum], CNF):-       
     fullAdder(X,Y,C,S,NextC, CNF1),
     add(Xs, Ys, NextC, Sum, CNF2),
     append(CNF1, CNF2, CNF).
 
 %we did in class, SUM of 2 Bits 
 %fullAdder(+,+,+,-,-, -)
-fullAdder(I1, I2, C_in, Out, C_out, CNF):-
-    %calc S
-    xor(X, I2, X), and( I1, I2, A1), and( X, C_in, A2),
-    xor( X, C_in, Out), or( A1, A2, C_out),
-
-    %calc NextC
-    xor(X, Y, Res), and(X, Y, A1), and(Res, C, A2), or(A1, A2, NextC),
-    CNF = [[X,Y,C,-S], [X, Y, -C, S], [X,-Y,C,S],[X,-Y,-C,-S],[-S,Y,C,S], [-X,Y,-C,-S],[-X,-Y,C,-S],[-X,-Y,-C,S]],
+fullAdder(I1, I2, C_in, S, C_out, CNF):-
+    %calc Next C
+    xor(I1, I2, X), 
+    and( I1, I2, A1), and( X, C_in, A2), or( A1, A2, C_out),
+    CNF = [[I1,I2,C_in,-S], [I1, I2, -C_in, S], [I1,-I2,C_in,S],[I1,-I2,-C_in,-S],[-S,I2,C_in,S], [-I1,I2,-C_in,-S],[-I1,-I2,C_in,-S],[-I1,-I2,-C_in,S]].
 
    
